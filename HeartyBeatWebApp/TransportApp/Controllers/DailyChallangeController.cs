3a42914b81b6f6e7cb654a28670d23a3e15aede5
Geri <85336812+GIBineva@users.noreply.github.com>
2024-06-17 20:34:47 +0300

    Added a countdown that shows when the next daily challenges will appear

--- HeartyBeatWebApp/TransportApp/Controllers/DailyChallangeController.cs
+++ HeartyBeatWebApp/TransportApp/Controllers/DailyChallangeController.cs
@@ -11,23 +11,23 @@ namespace HeartyBeatApp.Controllers
     {
         private readonly List<string> _challenges = new List<string>
         {
-            "Include fiber into your diet - Beans, Fruits, Oatmeal and Popcorn are foods full of fiber <3",
+            "Include fiber into your diet - Beans and Fruits and Oatmeal and Popcorn are foods full of fiber <3",
             "Make time for breakfast - The first meal for the day than can lead to positivity trough the day!",
             "Eat fish - lowers blood pressure and help reduce the risk of a heart attack or stroke!!",
-            "Eat nuts - good sources of protein, healthy fats, fibres, vitamins and minerals!!",
+            "Eat nuts - good sources of protein healthy fats fibres vitamins and minerals!!",
             "Limit your salt intake - can cause high blood pressure!!",
             "Reduce your saturated fat intake - too much saturated fat can cause cholesterol to build up in your arteries.",
             "Drink tea - May improve blood pressure and has so much more benefits",
             "Eat dark chocolate - as tasty as milk chocolate but ttimes more healthy <3",
             "Move your body throughout the day - WREK IT GIRL!!",
-            "Practice yoga - all you need is a yoga mat, youtube video and 5 minutes",
+            "Practice yoga - all you need is a yoga mat and an youtube video ",
             "Try strength training - helps with stress and you can benefit from the results!! ",
             "Try interval training - burns calories and reduces blood pressure",
             "Try dancing - fun way of working out!!",
             "Go for a walk - a nice way to clear your mind from stress",
             "Take the stairs - whoops the lift is suddently broken.. wonder who did that...",
             "Use housework as exercise - clean house and happy body <3",
-            "Be a kid - pfft who said this was for kids, enjoy your life!! ",
+            "Be a kid - pfft who said this was for kids and enjoy your life!! ",
             "Engage in hobbies - a nice way to spend some me time",
             "Laugh out loud - who cares if people hear you? There are 5 billion pple on earth",
             "Manage your stress - there are so many ways of that (breathing exercises and so much more)",
@@ -45,8 +45,22 @@ namespace HeartyBeatApp.Controllers
 
         public IActionResult Index()
         {
+            var today = DateTime.UtcNow.Date;
+            var lastAccessDate = HttpContext.Session.GetString("LastAccessDate");
             var dailyChallenges = HttpContext.Session.GetString("DailyChallenges");
             var rewardClaimed = HttpContext.Session.GetString("RewardClaimed");
+
+            if (string.IsNullOrEmpty(lastAccessDate) || DateTime.Parse(lastAccessDate) < today)
+            {
+                HttpContext.Session.SetString("LastAccessDate", today.ToString("yyyy-MM-dd"));
+                HttpContext.Session.Remove("DailyChallenges");
+                HttpContext.Session.Remove("RewardMessage");
+                HttpContext.Session.Remove("RewardImageUrl");
+                HttpContext.Session.Remove("RewardClaimed");
+
+                dailyChallenges = null;
+                rewardClaimed = null;
+            }
 
             if (string.IsNullOrEmpty(dailyChallenges))
             {
--- HeartyBeatWebApp/TransportApp/obj/Debug/net6.0/HeartyBeat.AssemblyInfo.cs
+++ HeartyBeatWebApp/TransportApp/obj/Debug/net6.0/HeartyBeat.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79045a9d70a61aaee6561f7a3e12771203919f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da1481b0740d82d5a6964b87f28ae628b0958e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
