ead383f67ef8fe57541d66fbb836fd68b4c612b9
Geri <85336812+GIBineva@users.noreply.github.com>
2024-06-20 22:23:12 +0300

    Made the AddYourTips page look like a chatbox, removed the prev Healthy tips page

--- HeartyBeatWebApp/TransportApp/obj/Debug/net6.0/HeartyBeat.AssemblyInfo.cs
+++ HeartyBeatWebApp/TransportApp/obj/Debug/net6.0/HeartyBeat.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd3d4b1ff5ecd6d0088757ff64f84a00761c19c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a03659983edd45c3fd04284a4cd97959058180")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeartyBeat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
